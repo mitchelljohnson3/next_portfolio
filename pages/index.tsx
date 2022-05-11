786157c0ca20486530ce5bb575f6e2f304995351
Mitchell-Johnson <mitchelljoho@gmail.com>
2022-05-11 10:53:21 -0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 
 const Home: NextPage = ({launches}) => {
-  console.log(', launches)
+  console.log('', launches)
   return (
     <div className={styles.container}>
       <Head>
